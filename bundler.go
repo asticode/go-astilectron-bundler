640215eab9e02bfb0c910424a92cb9084b70a0b6
Mark Laing <mark.laing@md.catapult.org.uk>
2020-04-27 10:26:04 +0100

    Adds build flags to bundler.

--- bundler.go
+++ bundler.go
@@ -52,6 +52,9 @@ type Configuration struct {
 	// Defaults to the current directory
 	InputPath string `json:"input_path"`
 
+	// Build flags to pass into go build
+	BuildFlags map[string]string `json:"build_flags"`
+
 	// LDFlags to pass through to go build
 	LDFlags LDFlags `json:"ldflags"`
 
@@ -120,6 +123,7 @@ type ConfigurationResourcesAdapter struct {
 type Bundler struct {
 	appName              string
 	bindPackage          string
+	buildFlags           map[string]string
 	cancel               context.CancelFunc
 	ctx                  context.Context
 	d                    *astikit.HTTPDownloader
@@ -197,6 +201,10 @@ func New(c *Configuration, l astikit.StdLogger) (b *Bundler, err error) {
 	}
 	if c.VersionElectron != "" {
 		b.versionElectron = c.VersionElectron
+	}
+
+	if len(c.BuildFlags) > 0 {
+		b.buildFlags = c.BuildFlags
 	}
 
 	// Add context
@@ -396,10 +404,23 @@ func (b *Bundler) bundle(e ConfigurationEnvironment) (err error) {
 		gp = build.Default.GOPATH
 	}
 
+	args := []string{"build", "-ldflags", std.String()}
+	var flag string
+	for k, v := range b.buildFlags {
+		if hasDash := strings.HasPrefix(k, "-"); hasDash {
+			flag = k
+		} else {
+			flag = "-" + k
+		}
+		args = append(args, flag, v)
+	}
+
+	var binaryPath = filepath.Join(environmentPath, "binary")
+	args = append(args, "-o", binaryPath, b.pathBuild)
+
 	// Build cmd
 	b.l.Debugf("Building for os %s and arch %s astilectron: %s electron: %s", e.OS, e.Arch, b.versionAstilectron, b.versionElectron)
-	var binaryPath = filepath.Join(environmentPath, "binary")
-	var cmd = exec.Command(b.pathGoBinary, "build", "-ldflags", std.String(), "-o", binaryPath, b.pathBuild)
+	var cmd = exec.Command(b.pathGoBinary, args...)
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env,
 		"GOARCH="+e.Arch,
